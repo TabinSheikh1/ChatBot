2514d96756c3f87c804378700b05b7b4fc4fe003
Tabin Sheikh <sheikhtabin@gmail.com>
2026-01-02 10:38:52 +0500

    pushing

---
+++ ingest.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+import os
+
+DATA_PATH = "data"
+DB_PATH = "vectordb"
+
+documents = []
+
+for file in os.listdir(DATA_PATH):
+    path = os.path.join(DATA_PATH, file)
+    if file.endswith(".pdf"):
+        loader = PyPDFLoader(path)
+        documents.extend(loader.load())
+    elif file.endswith(".txt"):
+        loader = TextLoader(path)
+        documents.extend(loader.load())
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+)
+
+chunks = splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+db = FAISS.from_documents(chunks, embeddings)
+db.save_local(DB_PATH)
+
+print("✅ Vector database created")
